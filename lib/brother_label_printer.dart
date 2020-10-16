3e7c015bb414462160385376210a3df13dcc59fb
Simone <stasimone95@gmail.com>
2020-10-16 16:48:34 +0200

    breaking change: named parameters

--- lib/brother_label_printer.dart
+++ lib/brother_label_printer.dart
@@ -3,27 +3,37 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class BrotherLabelPrinter {
-  static const MethodChannel _channel =
-      const MethodChannel('brother_label_printer');
+  static const MethodChannel _channel = const MethodChannel('brother_label_printer');
 
-  static Future<String> search(String macAddress, bool resetConnection) async {
-    final String result = await _channel.invokeMethod('search',
-        {'macAddress': macAddress, 'resetConnection': resetConnection});
-    return result;
-  }
-
-  static Future<String> transferTemplate(String filePath) async {
-    final String result = await _channel.invokeMethod('transferTemplate',
-        {'filePath': filePath});
+  static Future<String> search(
+    String macAddress, {
+    bool resetConnection,
+  }) async {
+    final String result = await _channel.invokeMethod('search', {
+      'macAddress': macAddress,
+      'resetConnection': resetConnection,
+    });
     return result;
   }
 
   static Future<String> printTemplate(
-      int templateId, Map<String, String> replacers, int numberOfCopies) async {
+    int templateId,
+    Map<String, String> replacers, {
+    String macAddress,
+    int numberOfCopies,
+  }) async {
     final String result = await _channel.invokeMethod('printTemplate', {
       'templateId': templateId,
       'replacers': replacers,
-      'numberOfCopies': numberOfCopies
+      'macAddress': macAddress,
+      'numberOfCopies': numberOfCopies,
+    });
+    return result;
+  }
+
+  static Future<String> transferTemplate(String filePath) async {
+    final String result = await _channel.invokeMethod('transferTemplate', {
+      'filePath': filePath,
     });
     return result;
   }
