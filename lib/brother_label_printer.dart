3539a2e612152c658704289c0eb60562e76be9b9
Tiziano <tiziano@purplesoft.io>
2021-07-01 00:02:46 +0200

    null safety

--- lib/brother_label_printer.dart
+++ lib/brother_label_printer.dart
@@ -3,13 +3,13 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class BrotherLabelPrinter {
-  static const MethodChannel _channel = const MethodChannel(
-      'brother_label_printer');
+  static const MethodChannel _channel = const MethodChannel('brother_label_printer');
 
-  static Future<String> search(String macAddress, {
-    bool resetConnection,
+  static Future<String?> search(
+    String macAddress, {
+    bool? resetConnection,
   }) async {
-    final String result = await _channel.invokeMethod('search', {
+    final String? result = await _channel.invokeMethod('search', {
       'macAddress': macAddress,
       'resetConnection': resetConnection,
     });
@@ -17,14 +17,15 @@ class BrotherLabelPrinter {
     return result;
   }
 
-  static Future<String> printTemplate(int templateId,
-      Map<String, String> replacers, {
-    String model,
-    String macAddress,
-        String ip,
-        int numberOfCopies,
-      }) async {
-    final String result = await _channel.invokeMethod('printTemplate', {
+  static Future<String> printTemplate(
+    int templateId,
+    Map<String, String> replacers, {
+    required String model,
+    required String macAddress,
+    required String ip,
+    int numberOfCopies = 1,
+  }) async {
+    final String? result = await _channel.invokeMethod('printTemplate', {
       'templateId': templateId,
       'model': model,
       'replacers': replacers,
@@ -32,11 +33,11 @@ class BrotherLabelPrinter {
       'ip': ip,
       'numberOfCopies': numberOfCopies,
     });
-    return result;
+    return result ?? '';
   }
 
-  static Future<String> transferTemplate(String filePath) async {
-    final String result = await _channel.invokeMethod('transferTemplate', {
+  static Future<String?> transferTemplate(String filePath) async {
+    final String? result = await _channel.invokeMethod('transferTemplate', {
       'filePath': filePath,
     });
     return result;
